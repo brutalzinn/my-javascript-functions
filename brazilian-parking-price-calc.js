62313d9c89d22143ae3dc26ffe4e8c4dab55038b
robertocpaes <31475696+brutalzinn@users.noreply.github.com>
2021-03-19 15:05:33 -0300

    Update brazilian-parking-price-calc.js

--- brazilian-parking-price-calc.js
+++ brazilian-parking-price-calc.js
@@ -1,38 +1,42 @@
 function diff_hours(dt2, dt1) {
-    var diff = new Date(dt2.getTime() - dt1.getTime());
-    return Math.abs(diff);
-  }
-  function separe_minutes(minutes) {
-  
-    var minutes_to_hour = minutes / 60;
-  
-    var minutesrest = (minutes_to_hour - Math.floor(minutes_to_hour)) * 60;
- 
-    return { horas: Math.floor(minutes_to_hour), minutos: Math.round(minutesrest) };
-  }
-  function calcPreco(tarifa, horas, minutos) {
-    var preco = tarifa
-    if(minutos > 15 && minutos <= 30){
-   preco = tarifa * 2
-   }
-   else if(minutos> 30 && minutos <= 45){
-   preco = tarifa * 3
-   }
-   else if(minutos > 45 && minutos <= 60){
-   preco = tarifa * 4
-   }
-   preco += tarifa * 4 * horas
-  return  parseFloat(preco)
-  }
+  var diff = new Date(dt2.getTime() - dt1.getTime());
+  return Math.abs(diff);
+}
+function separe_minutes(minutes) {
+
+  var minutes_to_hour = minutes / 60;
+
+  var minutesrest = (minutes_to_hour - Math.floor(minutes_to_hour)) * 60;
+
+  return { horas: Math.floor(minutes_to_hour), minutos: Math.round(minutesrest) };
+}
+function calcPreco(tarifa, horas, minutos) {
+  var preco = tarifa
+  if(minutos > 15 && minutos <= 30){
+ preco = tarifa * 2
+ }
+ else if(minutos> 30 && minutos <= 45){
+ preco = tarifa * 3
+ }
+ else if(minutos > 45 && minutos <= 60){
+ preco = tarifa * 4
+ }
+ preco += tarifa * 4 * horas
+return  parseFloat(preco)
+}
 function InitCalc(tarifa,entrada,saida){
-  var dateentrada = new Date(entrada); 
-  var datesaida = new Date(saida);
-  var diff = diff_hours(dateentrada, datesaida) / 1000 / 60;
-  var teste = separe_minutes(diff); 
-   console.log(teste.horas,teste.minutos)
+var dateentrada = new Date(entrada); 
+var datesaida = new Date(saida);
+var diff = diff_hours(dateentrada, datesaida) / 1000 / 60;
+var teste = separe_minutes(diff); 
+ console.log(teste.horas,teste.minutos)
 
 return calcPreco(
-  tarifa,
-  teste.horas,
-  teste.minutos)
+tarifa,
+teste.horas,
+teste.minutos)
+}
+
+module.exports = {
+  InitCalc
 }
